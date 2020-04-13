5ffe245036ad5293a39e10bd374464ed863bb32c
caposcar1998 <oscarcontrerascap@gmail.com>
2020-04-12 21:12:36 -0500

    Se crea tabs para administrador

---
+++ casaDeDia/imports/ui/tablasInformacion/TablaPromociones.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import MaterialTable from 'material-table';
+
+
+export default function TablaPromociones({ pacientes }) {
+
+
+
+
+    return (
+
+        <h1>TablaPromociones</h1>
+
+    );
+}
--- casaDeDia/imports/ui/tabs/AdministradorTab.jsx
+++ casaDeDia/imports/ui/tabs/AdministradorTab.jsx
@@ -1,10 +1,84 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import TablaPacientes from '../tablasInformacion/TablaPacientes';
+import TablaPromociones from '../tablasInformacion/TablaPromociones';
+import CrearEvento from "../crearCosas/CrearEvento";
+import CrearActividad from "../crearCosas/CrearActividad";
 
-
-export default function AdministradorTab (){
+export function TabPanel(props) {
+    const { children, value, index, ...other } = props;
 
     return (
-        <Button>Hola</Button>
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function AdministradorTab() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+                    <Tab label="Registrar paciente" {...a11yProps(0)} />
+                    <Tab label="Registrar evento" {...a11yProps(1)} />
+                    <Tab label="Registrar promocion" {...a11yProps(2)} />
+                    <Tab label="Registrar actividad" {...a11yProps(3)} />
+
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                <TablaPacientes/>
+      </TabPanel>
+            <TabPanel value={value} index={1}>
+                <CrearEvento/>
+      </TabPanel>
+            <TabPanel value={value} index={2}>
+                <TablaPromociones />
+      </TabPanel>
+            <TabPanel value={value} index={3}>
+                <CrearActividad/>
+      </TabPanel>
+        </div>
     );
 }
