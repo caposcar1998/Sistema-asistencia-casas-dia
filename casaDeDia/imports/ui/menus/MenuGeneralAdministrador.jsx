0b43c01e621f9dba105dc3d9f8a7ba467d318357
caposcar1998 <oscarcontrerascap@gmail.com>
2020-05-19 12:49:38 -0500

    Se anaden iconos faltantes

--- casaDeDia/imports/ui/menus/MenuGeneralAdministrador.jsx
+++ casaDeDia/imports/ui/menus/MenuGeneralAdministrador.jsx
@@ -9,7 +9,24 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListSubheader from "@material-ui/core/ListSubheader";
-import {DeckIcon,HouseIcon,GolfCourseIcon,StyleIcon,EmojiPeopleIcon,PeopleIcon,GradeIcon,WorkIcon,LocalLibraryIcon,DirectionsWalkIcon,RoomServiceIcon,LocalActivityIcon,EventSeatIcon,AssignmentIndIcon,RowingIcon,FavoriteIcon,LibraryBooksIcon,ExploreIcon} from "@material-ui/icons";
+import DeckIcon from '@material-ui/icons/Deck';
+import HouseIcon from '@material-ui/icons/House';
+import GolfCourseIcon from '@material-ui/icons/GolfCourse';
+import StyleIcon from '@material-ui/icons/Style';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import PeopleIcon from '@material-ui/icons/People';
+import GradeIcon from '@material-ui/icons/Grade';
+import WorkIcon from '@material-ui/icons/Work';
+import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import DirectionsWalkIcon from '@material-ui/icons/DirectionsWalk';
+import RoomServiceIcon from '@material-ui/icons/RoomService';
+import LocalActivityIcon from '@material-ui/icons/LocalActivity';
+import EventSeatIcon from '@material-ui/icons/EventSeat'
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import RowingIcon from '@material-ui/icons/Rowing';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import ExploreIcon from '@material-ui/icons/Explore';
 
 const useStyles = makeStyles({
   list: {
