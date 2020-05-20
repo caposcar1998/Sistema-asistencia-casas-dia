26923bff116b5e781be3aea2e7910525c965f95a
Raúl González Cardona <raul_5412@hotmail.com>
2020-05-20 15:56:06 -0500

    Listas de restricciones actualizadas

--- casaDeDia/imports/utilities/tablasEstaticas/restricciones.js
+++ casaDeDia/imports/utilities/tablasEstaticas/restricciones.js
@@ -1,14 +1,17 @@
 export const listaRestricciones = [
-"Edad 60 +",
-"Poder caminar",
-"Tener buena movilidad de su cuerpo",
-"Tener buena alimentación",
-"Contar con un buen descanso",
-"Tener higiéne",
-"No tener una infecciones respiratorias",
-"No tener problemas cardíacos",
-"No presentar padecimientos mentales",
-"Tener voluntad",
-"Ser independiente",
-"Expediente médico",
+"Ser mayor de 65 años de edad",
+"Residir en la Ciudad de México",
+"Si tiene dependencia moderada o severa, ser mayor de 60 años",
+"No tener sanciones en otra plaza pública",
+"Estar designada como persona dependiente vía médica o social",
+];
+
+export const restriccionesActividades = [
+    "Ser mayor de 60 años",
+    "No sufrir de mareos o pérdidas de equilibrio",
+    "Estar bajo vigilancia médica",
+    "Contar con una buena alimentación",
+    "Mantener una buena higiéne",
+    "Vestir ropa cómoda de tipo deportiva",
+    "Contar con toalla deportiva",
 ];
