d9be65b2100e9b03d2a4d94b9937994bb4698051
caposcar1998 <oscarcontrerascap@gmail.com>
2020-05-30 14:42:54 -0500

    Se crea molde para tarjtas

---
+++ casaDeDia/imports/ui/tarjetas/CrearTarjetas.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function CrearTarjetas() {
+    
+    return (
+        <div>Crear</div>
+    )
+}
---
+++ casaDeDia/imports/ui/tarjetas/TarjetasEfectivo.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Usuarios } from "../../api/usuarios/usuarios";
+import { Meteor } from 'meteor/meteor';
+import { tableIcons } from '../../utilities/TableIcons';
+
+
+function TarjetasEfectivo({ administradores }) {
+
+
+
+    return (
+
+        <MaterialTable
+            title="Tarjetas Salud"
+            icons={tableIcons}
+            columns={
+                [
+                    { title: "Nombre beneficiado", field: "nombre" },
+                    { title: "Tipo de tarjeta", field: "tipoTarjeta" },
+                    { title: "Numero Tarjeta", field: "numeroTarjeta" },
+                    { title: "curp", field: "curp" },
+                ]
+            }
+            data={administradores}
+
+        />
+    );
+}
+
+
+export default withTracker(() => {
+    Meteor.subscribe("usuarios");
+    return {
+        administradores: Usuarios.find({}).fetch(),
+    };
+})(TarjetasEfectivo);
