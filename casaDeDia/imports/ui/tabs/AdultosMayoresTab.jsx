82a1af1e5f16f93af41cc9ea83735e3107bdc311
caposcar1998 <oscarcontrerascap@gmail.com>
2020-05-30 21:09:10 -0500

    Se elimina tabla dulto mayor

--- casaDeDia/imports/ui/tabs/AdultosMayoresTab.jsx
+++ casaDeDia/imports/ui/tabs/AdultosMayoresTab.jsx
@@ -6,7 +6,6 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import TablaAdultoMayor from '../tablasInformacion/TablaAdultoMayor';
 import AdultoMayor  from '../adultosMayores/AdultoMayor';
 
 
