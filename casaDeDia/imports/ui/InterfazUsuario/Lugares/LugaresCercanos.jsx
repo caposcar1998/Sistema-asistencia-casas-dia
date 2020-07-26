e082c0825ffa912087c479f9e344bb0aba471319
caposcar1998 <oscarcontrerascap@gmail.com>
2020-07-25 19:32:21 -0500

    Se llaman casas de dia cercanos

--- casaDeDia/imports/ui/InterfazUsuario/Lugares/LugaresCercanos.jsx
+++ casaDeDia/imports/ui/InterfazUsuario/Lugares/LugaresCercanos.jsx
@@ -1,10 +1,26 @@
 import React, {useEffect} from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Grid, Typography } from "@material-ui/core";
 import { useState } from "react";
+import TarjetasLugares from "./TarjetasLugares";
+
 
 
 const useStyles = makeStyles((theme) => ({
 
+    fotoPrincipal: {
+        width: "100%",
+        height: "200px",
+        backgroundImage: "url('/fotos/Fondo1.jpg')"
+    },
+    root: {
+        maxWidth: 345,
+    },
+    espacio: {
+        marginBottom: theme.spacing(2)
+    },
+    titulo: {
+        color: "white"
+    }
 }));
 
 
@@ -69,7 +85,84 @@ export default function LugaresCercanos({codigoPostal}) {
                     });
             }
         )
-     }
+    }
+    
+    function seleccionDeLugar(casaSeleccionada) {
+        ruta = "Lugar/".concat(casaSeleccionada._id)
+        FlowRouter.go(ruta)
+    }
 
-    return (<>{codigoPostal}</>)
+    return (
+
+        <Grid container>
+
+            <Grid item xs={12} className={classes.fotoPrincipal}>
+                <Grid container
+                    direction="row"
+                    justify="center"
+                    alignItems="center">
+                    <Typography variant="h1" className={classes.titulo}>Servicios cercanos</Typography>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Grid item xs={12}>
+                        <Typography variant="h1">Casas de dia</Typography>
+                    </Grid>
+                    {casasDia.map((lugar) => (
+                        <Grid item xs={6}>
+                            <TarjetasLugares
+                                lugar={lugar}
+                                seleccionDeLugar={seleccionDeLugar}
+                            />
+                        </Grid>
+                    ))}
+
+                    <Grid item xs={12}>
+                        <Typography variant="h1">Asilos</Typography>
+                    </Grid>
+                    {casasDia.map((lugar) => (
+                        <Grid item xs={6}>
+                            <TarjetasLugares
+                                lugar={lugar}
+                                seleccionDeLugar={seleccionDeLugar}
+                            />
+                        </Grid>
+                    ))}
+
+                    <Grid item xs={12}>
+                        <Typography variant="h1">Clubes</Typography>
+                    </Grid>
+                    {casasDia.map((lugar) => (
+                        <Grid item xs={6}>
+                            <TarjetasLugares
+                                lugar={lugar}
+                                seleccionDeLugar={seleccionDeLugar}
+                            />
+                        </Grid>
+                    ))}
+
+                    <Grid item xs={12}>
+                        <Typography variant="h1">Colectivos</Typography>
+                    </Grid>
+                    {casasDia.map((lugar) => (
+                        <Grid item xs={6}>
+                            <TarjetasLugares
+                                lugar={lugar}
+                                seleccionDeLugar={seleccionDeLugar}
+                            />
+                        </Grid>
+                    ))}
+
+                </Grid>
+            </Grid>
+
+
+        </Grid>
+    )
  }
